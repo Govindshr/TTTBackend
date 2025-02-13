85b1be746038627df15273650b8c09f2f86596af
Govind Shringi <govind.shringi@qdegrees.org>
2025-02-13 10:28:22 +0530

    updates

--- db/config.js
+++ db/config.js
@@ -5,8 +5,9 @@ mongoose.set("strictQuery", true);
 
 // let url = "mongodb+srv://tolet:tolet@cluster0.nlbdxil.mongodb.net/kms_db?retryWrites=true&w=majority"
 // let url = "mongodb+srv://akssmbr91:CqGu8uIfp2Hhs4hC@cluster0.mtbcggb.mongodb.net/kms_db?retryWrites=true&w=majority"
-let url = "mongodb+srv://singhkumaramit2019:cGiOlc0Xd5yG6Yhs@cluster0.jvsyd.mongodb.net/travelers?retryWrites=true&w=majority&appName=Cluster0"
+// let url = "mongodb+srv://singhkumaramit2019:cGiOlc0Xd5yG6Yhs@cluster0.jvsyd.mongodb.net/travelers?retryWrites=true&w=majority&appName=Cluster0"
 // let url = "mongodb://localhost:27017/kms_db"
+let url = "mongodb://trippingtales:w2L$B3qjI6c$KQ@3.110.118.223:27017/test"
 
 mongoose.connect(url,
 {useNewUrlParser : true},(err,result)=>{
